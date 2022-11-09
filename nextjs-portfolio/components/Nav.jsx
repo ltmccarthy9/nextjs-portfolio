0c228f141bbc5418dcb33bd0d06fd41a11cc8674
Liam McCarthy <ltmccarthy9@gmail.com>
2022-11-09 16:55:59 -0600

    nav mobile w/out functionality

--- nextjs-portfolio/components/Nav.jsx
+++ nextjs-portfolio/components/Nav.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-
+import {AiOutlineClose, AiOutlineMenu, AiOutlineMail} from 'react-icons/ai';
+import {FaLinkedinIn, FaGithub} from 'react-icons/fa';
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Nav = () => {
     return (
@@ -26,8 +28,62 @@ const Nav = () => {
                         <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                     </Link>
                 </ul>
+                <div className='md:hidden'>
+                    <AiOutlineMenu size={25} />
+                </div>
+            </div>
             </div>
 
+            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+                    <div>
+                        <div className='flex w-full items-center justify-between'>
+                            <Image src="" alt='/' width='87' height='35' />
+                            <div className='rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer'>
+                                <AiOutlineClose />
+                            </div>
+                        </div>
+                        <div className='border-b border-gray-300 my-4'>
+                            <p className='w-[85%] md:w-[90%] py-4'>Let's build something</p>
+                        </div>
+                    </div>
+                    <div className='py-4 flex-col'>
+                        <ul className='uppercase'>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Home</li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>About</li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Skills</li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Project</li>
+                            </Link>
+                            <Link href='/'>
+                            <li className='py-4 text-sm'>Contact</li>
+                            </Link>
+                        </ul>
+                        <div className='pt-40'>
+                            <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+                            <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaGithub/>
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <AiOutlineMail />
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );

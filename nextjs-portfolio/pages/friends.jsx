76a3f303147c3ca0e07de3e5124804ef416ab888
Liam McCarthy <ltmccarthy9@gmail.com>
2023-01-22 12:43:47 -0600

    title fix and description for friends

--- nextjs-portfolio/pages/friends.jsx
+++ nextjs-portfolio/pages/friends.jsx
@@ -13,15 +13,15 @@ const friends = () => {
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10'/>
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={friendsImg} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-            <h2 className='py-2'>Friend's First</h2>
-            <h3>React / MongoDB / Express / Nodejs</h3>
+            <h2 className='py-2'>Friends First</h2>
+            <h3>Reactjs / MongoDB / Mongoose/ Expressjs / Nodejs</h3>
         </div>
       </div>
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
         <div className='col-span-4'>
             <p className='text-xl tracking-widest uppercase text-[#3fb88e] py-3'>Project Overview</p>
-            <p>My personal take on a dating application.
+            <p>My take on a dating and friend-making application focused around events and meeting in person first.
             </p>
             <button onClick={goRepo} className='px-8 py-2 mt-4 hover:scale-110 ease-in duration-200'>Code Repo</button>
         </div>

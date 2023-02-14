fbd0b61ef3e76ca8af026efcca189ac9e3dfc44f
Liam McCarthy <ltmccarthy9@gmail.com>
2023-02-13 23:38:33 -0600

    desc

--- nextjs-portfolio/components/About.jsx
+++ nextjs-portfolio/components/About.jsx
@@ -10,11 +10,10 @@ const About = () => {
             <p className='uppercase text-xl tracking-widest text-[#3fb88e]'>About</p>
             <h2 className='py-4'>Who I Am</h2>
             <p className='py-2 text-gray-600'>
-            Full stack web developer with a degree in economics. 
-            I recently earned a certificate in full stack web development 
+            Full stack web developer with an undergraduate degree in economics. 
+            In September of 2022 earned a certificate in full stack web development 
             from a boot camp by the University of California - Berkeley. 
-            As a graduate of this boot camp, I have over a hundred hours of 
-            experience building full-stack applications both individually and in teams. 
+            I have over a thousand hours of experience building full-stack applications both individually and in teams. 
             Beyond Javascript and HTML5/CSS, I am experienced in React, Redux, mySQL, mongoDB, 
             REST APIs, ORMs, git workflow, and more. I am a quick learner with strong 
             analytical skills as well as a deeply creative side. I’m excited to bring my 

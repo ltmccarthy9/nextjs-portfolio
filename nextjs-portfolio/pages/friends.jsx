54a1188b48e04a283f44cc643836eedea930b919
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-03 13:25:03 -0600

    ff video

--- nextjs-portfolio/pages/friends.jsx
+++ nextjs-portfolio/pages/friends.jsx
@@ -31,7 +31,7 @@ const friends = () => {
               </li>
             </ul>
             <div className='my-12'>
-              <iframe className='m-auto' width="1280" height="720" src="https://www.youtube.com/embed/AgHvGWZngOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              <iframe className='m-auto' width="1280" height="720" src="https://www.youtube.com/embed/DTRD7qGdLQE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
       </div>

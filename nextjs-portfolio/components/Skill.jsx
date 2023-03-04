cf8dd98704c3316eb726af2afda46457a6b98f3e
Liam McCarthy <ltmccarthy9@gmail.com>
2023-03-04 12:11:03 -0600

    logo colors

--- nextjs-portfolio/components/Skill.jsx
+++ nextjs-portfolio/components/Skill.jsx
@@ -4,14 +4,11 @@ import React from 'react'
 const Skill = ({skill, icon}) => {
   return (
     <div className='bg-[#f5f8f8] p-6 border border-gray-200 rounded-xl'>
-        <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+        <div className='flex flex-col gap-1 justify-center'>
             <div className='m-auto'>
                 {icon}
-                </div>
-
-                <div className='flex flex-col items-center justify-center'>
-                    <h3>{skill}</h3>
             </div>
+            <h3 className='m-auto'>{skill}</h3>
         </div>
     </div>
   )
--- nextjs-portfolio/components/Skills.jsx
+++ nextjs-portfolio/components/Skills.jsx
@@ -14,22 +14,22 @@ const Skills = () => {
     <div id='skills' className='w-full lg:h-screen px-2 py-2 pb-72'>
         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
             <p className='text-2xl tracking-widest uppercase text-[#f4af61] mb-2'>Skills</p>
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2'>
-               <Skill skill='Javascript' icon={<SiJavascript size={35}/>} />
-               <Skill skill='Typescript' icon={<SiTypescript size={35} />} />
-               <Skill skill='HTML5' icon={<AiFillHtml5 size={35}/>}/>
-               <Skill skill='CSS' icon={<SiCss3 size={35} />}/>
-               <Skill skill='React' icon={<FaReact size={35}/>}/>
-               <Skill skill='Redux' icon={<SiRedux size={35} />} />
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2'>
+               <Skill skill='Javascript' icon={<SiJavascript className='bg-[#323330]' color='#F0DB4F' size={35}/>} />
+               <Skill skill='Typescript' icon={<SiTypescript className='bg-[#ffffff]' color='#007acc' size={35} />} />
+               <Skill skill='HTML' icon={<AiFillHtml5 color='#f06529' size={35}/>}/>
+               <Skill skill='CSS' icon={<SiCss3 color='#2965f1' size={35} />}/>
+               <Skill skill='React' icon={<FaReact color='#61DBFB' size={35}/>}/>
+               <Skill skill='Redux' icon={<SiRedux color='#764abc' size={35} />} />
                <Skill skill='NextJS' icon={<TbBrandNextjs size={35} />} />
-               <Skill skill='Nodejs' icon={<FaNodeJs size={35} />}/>
+               <Skill skill='Nodejs' icon={<FaNodeJs color='#3c873a' size={35} />}/>
                <Skill skill='express.js' icon={<SiExpress size={35} />}/>
-               <Skill skill='PostreSQL' icon={<SiPostgresql size={35} />} />
-               <Skill skill='MySQL' icon={<GrMysql size={35} />}/>
-               <Skill skill='MongoDB' icon={<SiMongodb size={35} />} />
-               <Skill skill='Firebase' icon={<SiFirebase size={35}/>} />
-               <Skill skill='Bootstrap' icon={<SiBootstrap size={35} />} />
-               <Skill skill='TailwindCSS' icon={<SiTailwindcss size={35} />}/>
+               <Skill skill='PostreSQL' icon={<SiPostgresql color='#336791' size={35} />} />
+               <Skill skill='MySQL' icon={<GrMysql color='#00758f' size={35} />}/>
+               <Skill skill='MongoDB' icon={<SiMongodb color='#4DB33D' size={35} />} />
+               <Skill skill='Firebase' icon={<SiFirebase color='#F5820D' size={35}/>} />
+               <Skill skill='Bootstrap' icon={<SiBootstrap color='#563d7c' size={35} />} />
+               <Skill skill='TailwindCSS' icon={<SiTailwindcss color='#4dc0b5' size={35} />}/>
                <Skill skill='ORMs/ORDs' icon={<BiTable size={35} />}/>
             </div>
         </div>
